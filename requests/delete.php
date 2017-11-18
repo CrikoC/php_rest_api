31a60d58ce8fe62f2628a622a9c86cc8ab860908
Kostas Christopoulos <konstadinoschr@gmail.com>
2017-11-18 17:47:26 +0200

    Minor refactoring v 3.0 just keep them comming

--- requests/delete.php
+++ requests/delete.php
@@ -1,55 +1,59 @@
 <?php
-if(URL == "auth") {
-    if(isset($_GET['token'])) {
-        $token = $_GET['token'];
-        $user = User::find_single_by_column('token', $token);
-        $user->token = "";
-        $user->update();
+switch (URL) {
+    case "auth":
+        if(isset($_GET['token'])) {
+            $token = $_GET['token'];
+            $user = User::find_single_by_column('token', $token);
+            $user->token = "";
+            $user->update();
 
-        setcookie("LC", "", time()-3600);
+            setcookie("LC", "", time()-3600);
 
-        echo '{ "Status" : "Logged out" }';
-        http_response_code(200);
-    } else {
-        echo '{ "Error" : "Mal-formed request" }';
-        http_response_code(405);
-    }
-} else if(URL == "users") {
-    if(isset($_COOKIE['LC'])) {
-        $login_cookie = $_COOKIE['LC'];
+            echo '{ "Status" : "Logged out" }';
+            http_response_code(200);
+        } else {
+            echo '{ "Error" : "Mal-formed request" }';
+            http_response_code(405);
+        }
+        break;
+    case "users":
+        if(isset($_COOKIE['LC'])) {
+            $login_cookie = $_COOKIE['LC'];
 
-        if(isset($_GET['id'])) {
-            $id = $_GET['id'];
-            $user = User::find_by_id($id);
+            if(isset($_GET['id'])) {
+                $id = $_GET['id'];
+                $user = User::find_by_id($id);
 
-            if($user->token == $login_cookie) {
-                setcookie("LC", "", time()-3600);
+                if($user->token == $login_cookie) {
+                    setcookie("LC", "", time()-3600);
+                }
+                $user->delete();
+                echo '{ "Status" : "User deleted" }';
+                http_response_code(200);
+            } else {
+                echo '{ "Error" : "Mal-formed request" }';
+                http_response_code(405);
             }
-            $user->delete();
-            echo '{ "Status" : "User deleted" }';
-            http_response_code(200);
         } else {
-            echo '{ "Error" : "Mal-formed request" }';
-            http_response_code(405);
+            echo '{ "Error" : Not authorized" }';
+            http_response_code(401);
         }
-    } else {
-        echo '{ "Error" : Not authorized" }';
-        http_response_code(401);
-    }
-} else if(URL == "posts") {
-    if(isset($_COOKIE['LC'])) {
-        if (isset($_GET['id'])) {
-            $id = $_GET['id'];
-            $post = Post::find_by_id($id);
-            $post->delete();
-            echo '{ "Status" : "Post deleted" }';
-            http_response_code(200);
+        break;
+    case "posts":
+        if(isset($_COOKIE['LC'])) {
+            if (isset($_GET['id'])) {
+                $id = $_GET['id'];
+                $post = Post::find_by_id($id);
+                $post->delete();
+                echo '{ "Status" : "Post deleted" }';
+                http_response_code(200);
+            } else {
+                echo '{ "Error" : "Mal-formed request" }';
+                http_response_code(405);
+            }
         } else {
-            echo '{ "Error" : "Mal-formed request" }';
-            http_response_code(405);
+            echo '{ "Error" : Not authorized" }';
+            http_response_code(401);
         }
-    } else {
-        echo '{ "Error" : Not authorized" }';
-        http_response_code(401);
-    }
+        break;
 }
--- requests/post.php
+++ requests/post.php
@@ -1,59 +1,61 @@
 <?php
-if (URL == "auth") {
-    $postBody = json_decode(INPUT);
-
-    $user = User::find_single_by_column("username", $postBody[0]->username);
-
-    if (password_verify($postBody[1]->password, $user->password)) {
-        $cstrong = True;
-
-        $user->token = bin2hex(openssl_random_pseudo_bytes(64, $cstrong));
-        $user->update();
-
-        setcookie("LC", $user->token, time()+3600);
-
-        echo '{ "Token": "' . $user->token . '" }';
-    } else {
-        echo "Incorrect username or password";
-        http_response_code(401);
-    }
-}
-if (URL == "users") {
-    $postBody = json_decode(INPUT);
-
-    $user = new User();
-    $user->username = $postBody[0]->username;
-    $user->password = password_hash($postBody[1]->password, PASSWORD_BCRYPT, ['cost' => 10]);
-
-    if ($user->create()) {
-        echo '{ "Status" : "User added" }';
-        http_response_code(200);
-    } else {
-        echo '{ "Error" : "Mal-formed request" }';
-        http_response_code(405);
-    }
-}
-if (URL == "posts") {
-    if(isset($_COOKIE['LC'])) {
-        $login_cookie = $_COOKIE['LC'];
-        $author = User::find_single_by_column('token', $login_cookie);
-
+switch (URL) {
+    case "auth":
         $postBody = json_decode(INPUT);
 
-        $post = new Post();
-        $post->title = $postBody[0]->title;
-        $post->body = $postBody[1]->body;
-        $post->author = $author->username;
+        $user = User::find_single_by_column("username", $postBody[0]->username);
 
-        if ($post->create()) {
-            echo '{ "Status" : "Post added" }';
+        if (password_verify($postBody[1]->password, $user->password)) {
+            $cstrong = True;
+
+            $user->token = bin2hex(openssl_random_pseudo_bytes(64, $cstrong));
+            $user->update();
+
+            setcookie("LC", $user->token, time()+3600);
+
+            echo '{ "Token": "' . $user->token . '" }';
+        } else {
+            echo "Incorrect username or password";
+            http_response_code(401);
+        }
+        break;
+    case "users":
+        $postBody = json_decode(INPUT);
+
+        $user = new User();
+        $user->username = $postBody[0]->username;
+        $user->password = password_hash($postBody[1]->password, PASSWORD_BCRYPT, ['cost' => 10]);
+
+        if ($user->create()) {
+            echo '{ "Status" : "User added" }';
             http_response_code(200);
         } else {
             echo '{ "Error" : "Mal-formed request" }';
             http_response_code(405);
         }
-    } else {
-        echo '{ "Error" : Not authorized" }';
-        http_response_code(401);
-    }
+        break;
+    case "posts":
+        if(isset($_COOKIE['LC'])) {
+            $login_cookie = $_COOKIE['LC'];
+            $author = User::find_single_by_column('token', $login_cookie);
+
+            $postBody = json_decode(INPUT);
+
+            $post = new Post();
+            $post->title = $postBody[0]->title;
+            $post->body = $postBody[1]->body;
+            $post->author = $author->username;
+
+            if ($post->create()) {
+                echo '{ "Status" : "Post added" }';
+                http_response_code(200);
+            } else {
+                echo '{ "Error" : "Mal-formed request" }';
+                http_response_code(405);
+            }
+        } else {
+            echo '{ "Error" : Not authorized" }';
+            http_response_code(401);
+        }
+        break;
 }
