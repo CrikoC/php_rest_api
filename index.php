c794b20c13c77f1c7447e5b503065002ea735cb2
Kostas Christopoulos <konstadinoschr@gmail.com>
2017-12-01 22:48:03 +0200

    Adding php-jwt for authentication

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
 include "includes/initialize.php";
+require_once "vendor/autoload.php";
 
 switch (METHOD) {
     case "GET":
