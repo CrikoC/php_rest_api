6dcff18a469eeaa866c81f83babf548ef783d860
CrikoC <konstadinoschr@gmail.com>
2018-06-21 14:59:22 +0300

    adding

--- index.php
+++ index.php
@@ -20,6 +20,15 @@ switch (METHOD) {
     case "DELETE":
         include "requests/delete.php";
         break;
+    case "OPTIONS":
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            // may also be using PUT, PATCH, HEAD etc
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
     default:
         http_response_code(405);
 }
